bc918ae64237411777f317f62b6dd0db583ee751
BarbaraCortes <barbaracortes.98@hotmail.com>
2018-05-26 15:55:33 -0300

    mudei coisinhas

--- a.py
+++ a.py
@@ -2,118 +2,147 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
+
+# Variavel global que define o incremento
+increment = 3.47
 # Variavel global que define o tipo de projecao
 op = 0
 # Variaveis globais que definem os angulos de rotacao nos eixos x, y e z
 angle_x = 0
 angle_y = 0
 angle_z = 0
+# Variaveis globais que definem as posicoes de translacao nos eixos x, y e z
+pos_x = 0
+pos_y = 0
+pos_z = 0
 
 def init():
-    glutInit()
-    glutInitWindowSize(500, 500)
-    glutCreateWindow("Teapot")
+	glutInit()
+	glutInitWindowSize(500, 500)
+	glutCreateWindow("Teapot")
 
-    glClearColor(1, 1, 1, 0)
+	glClearColor(1, 1, 1, 0)
 
 def setProjection():
 	# Define que ira trabalhar com a matriz de projecao
-    glMatrixMode(GL_PROJECTION)
-    # Carrega a matriz identidade
-    glLoadIdentity()
+	glMatrixMode(GL_PROJECTION)
+	# Carrega a matriz identidade
+	glLoadIdentity()
 
-    # Projecao ortogonal
-    if (op == 0):
-    	# x_min, x_max, y_min, y_max, z_min, z_max 
-    	glOrtho(-2, 2, -2, 2, -20, 20)
-    # Projecao perspectiva
-    else:
-    	# x_min, x_max, y_min, y_max, z_near, z_far
-    	glFrustum(-1, 1, -1, 1, 1, 100)
+	# Projecao ortogonal
+	if (op == 0):
+		# x_min, x_max, y_min, y_max, z_min, z_max 
+		glOrtho(-20, 20, -20, 20, -200, 200)
+	# Projecao perspectiva
+	else:
+		# x_min, x_max, y_min, y_max, z_near, z_far
+		glFrustum(-0.5, 0.5, -0.5, 0.5, 0.5, 50)
 
 # Desenha os eixos x, y e z em degrade
 def drawAxis() :
-    # Eixo x -> vermelho
-    glBegin(GL_LINES)
-    glColor3f(1, 0, 0)
-    glVertex3f(0,0,0)
-    glColor3f(1, 1, 1)
-    glVertex3f(2,0,0)
-    glEnd()
-    
-    # Eixo y -> amarelo
-    glColor3f(1, 1, 0)
-    glBegin(GL_LINES)
-    glVertex3f(0,0,0)
-    glColor3f(1, 1, 1)
-    glVertex3f(0,2,0)
-    glEnd()
+	# Eixo x -> vermelho
+	glBegin(GL_LINES)
+	glColor3f(1, 0, 0)
+	glVertex3f(0,0,0)
+	glColor3f(1, 1, 1)
+	glVertex3f(20,0,0)
+	glEnd()
+	
+	# Eixo y -> amarelo
+	glColor3f(1, 1, 0)
+	glBegin(GL_LINES)
+	glVertex3f(0,0,0)
+	glColor3f(1, 1, 1)
+	glVertex3f(0,20,0)
+	glEnd()
 
-    # Eixo z -> azul
-    glColor3f(0, 0, 1)
-    glBegin(GL_LINES)
-    glVertex3f(0,0,0)
-    glColor3f(1, 1, 1)
-    glVertex3f(0,0,2)
-    glEnd()
-    
+	# Eixo z -> azul
+	glColor3f(0, 0, 1)
+	glBegin(GL_LINES)
+	glVertex3f(0,0,0)
+	glColor3f(1, 1, 1)
+	glVertex3f(0,0,20)
+	glEnd()
+	
 # Funcao para capturar os eventos do teclado
 def keyPressEvent(key, x, y) :
-    global increment, toggleAnimation
+	global angle_x, angle_y, angle_z, increment, pos_x, pos_y, pos_z
 
-    if key == '\x1b':
-        exit(0) # Sai do programa se apertar ESC
+	if key == '\x1b':
+		exit(0) # Sai do programa se apertar ESC
+	
+	if (key == 'Q'):
+		angle_x += increment # Rotaciona em x no sentido anti horario
+	elif (key == 'A'):
+		angle_x -= increment # Rotaciona em x no sentido horario
+	elif (key == 'W'):
+		angle_y += increment # Rotaciona em y no sentido anti horario
+	elif (key == 'S'):
+		angle_y -= increment # Rotaciona em y no sentido horario
+	elif (key == 'E'):
+		angle_z += increment # Rotaciona em z no sentido anti horario
+	elif (key == 'D'):
+		angle_z -= increment # Rotaciona em z no sentido horario
+	else: 
+		pass
+	
 	if (key == 'q'):
-		angle_x += 1 # Rotaciona em x no sentido anti horario
+		pos_x += increment # Rotaciona em x no sentido anti horario
 	elif (key == 'a'):
-		angle_x -= 1 # Rotaciona em x no sentido horario
+		pos_x -= increment # Rotaciona em x no sentido horario
 	elif (key == 'w'):
-		angle_y += 1 # Rotaciona em y no sentido anti horario
+		pos_y += increment # Rotaciona em y no sentido anti horario
 	elif (key == 's'):
-		angle_y -= 1 # Rotaciona em y no sentido horario
+		pos_y -= increment # Rotaciona em y no sentido horario
 	elif (key == 'e'):
-		angle_z += 1 # Rotaciona em z no sentido anti horario
+		pos_z += increment # Rotaciona em z no sentido anti horario
 	elif (key == 'd'):
-		angle_z -= 1 # Rotaciona em z no sentido horario
+		pos_z -= increment # Rotaciona em z no sentido horario
 	else: 
 		pass
 
 	display()
 
 def display():
-    glClear(GL_COLOR_BUFFER_BIT)
+	glClear(GL_COLOR_BUFFER_BIT)
 
-    # Define uma porta de visao para a projecao ortogonal
-    glViewport(0, 0, 500, 500)
+	# Define uma porta de visao para a projecao ortogonal
+	glViewport(0, 0, 500, 500)
 
-    # Chama a funcao para configurar o tipo de projecao ortogonal
-    setProjection()
+	# Chama a funcao para configurar o tipo de projecao ortogonal
+	setProjection()
 
-    # Define que ira trabalhar com a matriz de modelo/visao
-    glMatrixMode(GL_MODELVIEW)
+	# Define que ira trabalhar com a matriz de modelo/visao
+	glMatrixMode(GL_MODELVIEW)
 
-    # Carrega a matriz identidade
-    glLoadIdentity()
-    
-    # Define as configuracoes do observador
-    gluLookAt(0, 0, 2, 0, 0, 0, 0, 1, 0)
-    
-    # Desenha os eixos 
-    drawAxis()
+	# Carrega a matriz identidade
+	glLoadIdentity()
+	
+	# Define as configuracoes do observador
+	gluLookAt(2, 2, 2, 0, 0, 0, 0, 1, 0)
+	
+	
+# Desenha os eixos 
+	drawAxis()
+	# Rotaciona o objeto
+	global pos_x, pos_y, pos_z
+	global angle_x, angle_y, angle_z
+	
+	glTranslatef(0, 0, 0)
+	glRotatef(angle_x, 1, 0, 0)
+	glRotatef(angle_y, 0, 1, 0)
+	glRotatef(angle_z, 0, 0, 1)
+	#glTranslatef(pos_x, pos_y, pos_z)
 
-    print angle_x
-    print angle_y
-    print angle_z
-    # Rotaciona o objeto
-    glRotatef(angle_x, 1, 0, 0)
-    glRotatef(angle_y, 0, 1, 0)
-    glRotatef(angle_z, 0, 0, 1)
 
-    # Desenha um bule de arame na cor verde
-    glColor3f(0,1,0)
-    glutWireTeapot(1.0)
+	# Translada o objeto
+	glTranslatef(pos_x, pos_y, pos_z)
 
-    glFlush()
+	# Desenha um bule de arame na cor verde
+	glColor3f(0,1,0)
+	glutWireTeapot(10.0)
+
+	glFlush()
 
 
 print("Qual o tipo de projecao desejada?")
